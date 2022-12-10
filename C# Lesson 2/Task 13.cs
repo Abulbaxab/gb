cf0314c39f3a1c43d526100181e3cb3eccc04c1e
Abulbaxab <60078230+Abulbaxab@users.noreply.github.com>
2022-12-10 12:25:34 +0300

    Add files via upload

---
+++ C# Lesson 2/Task 13.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = ReadInt("Введите число: ");
+            int count = number.ToString().Length;
+            Console.Write(CheckNum(number, count));
+
+            int ReadInt(string message)
+            {
+                Console.Write(message);
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+            int CheckNum(int a, int b)
+            {
+                int result = 0;
+                if (b < 3)
+                {
+                    Console.Write("Третьей цифры нет, получи ноль: ");
+                }
+                else
+                {
+                    int c = 1;
+                    for (int i = b; i > 3; i--)
+                    {
+                        c = c * 10;
+                    }
+
+                    result = (a / c) % 10;
+                }
+                return result;
+            }
+        }
+        }
+    }
+}
---
+++ C# Lesson 2/Task 15.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayNumber = ReadInt("Введите число от 1 до 7: ");
+            Console.WriteLine(Day(dayNumber));
+
+            int ReadInt(string message)
+            {
+                Console.Write(message);
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+            string Day(int a)
+            {
+                if (a > 0 && a < 8)
+                {
+                    if (a == 7 || a == 6)
+                    {
+                        Console.Write("Под цифрой " + a + " - Выходной");
+                    }
+                    else
+                    {
+                        Console.Write("Под цифрой " + a + " - Рабочий");
+                    }
+                }
+                else
+                {
+                    Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому невозможно определить");
+                }
+                return " день.";
+            }
+        }
+    }
+}
